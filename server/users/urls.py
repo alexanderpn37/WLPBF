61eb3c7a514bafac0ab7e91b7e340f9b660556c7
alexanderpn37 <alexanderpn@gmail.com>
2024-10-30 01:19:14 -0400

    listo el user

--- server/users/urls.py
+++ server/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    re_path('singup', views.singup),
+    re_path('signup', views.signup),
     re_path('login/', obtain_auth_token, name='login'),
     re_path('test-token', views.test_token),
 ]
--- server/users/views.py
+++ server/users/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.response import Response
 from django.contrib.auth.models import User
@@ -22,17 +22,18 @@ def api_login(request):
     serializer = UserSerializer(user)
     return Response({"token": token.key , "user":serializer.data}, status=status.HTTP_200_OK)
 
+
 @api_view(['POST'])
-def singup(request):
+@permission_classes([AllowAny])
+def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        user = User.objects.get(email=serializer.data['email'])
+        user = serializer.save()
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'token': token.key, 'user': serializer.data}, status=201)
+    return Response(serializer.errors, status=400)
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication,TokenAuthentication])
